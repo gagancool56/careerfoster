d59d59db9e8d38f09168952fcf0c5121d73dbf4b
gagancool56 <gagancool56@gmail.com>
2021-03-14 21:37:04 +0530

    new change in more about us and more pages

--- application/views/more.php
+++ application/views/more.php
@@ -1,4 +1,4 @@
-<?php include(APPPATH.'views/header.php') ?>
+<?php include(APPPATH . 'views/header.php') ?>
 <!--? Hero Start -->
 <div class="slider-area2">
     <div class="slider-height2 hero-overly2 d-flex align-items-center">
@@ -16,45 +16,45 @@
 <!-- Hero End -->
 
 <div class="about-details section-padding30">
-            <div class="container">
-                <div class="row">
-                    <div class="offset-xl-1 col-lg-8">
-                  <div class="about-details-cap mb-50">
-  <h4>Multiple Hiring Options</h4>
-  <ul class="unordered-list">
-  <li> Permanent hire</li>
-<li>Fixed-term contracts</li>
-<li>Casual-contracts</li>
-</ul>
+    <div class="container">
+        <div class="row">
+            <div class="offset-xl-1 col-lg-8">
+                <div class="about-details-cap mb-50">
+                    <h4>Multiple Hiring Options</h4>
+                    <ul class="unordered-list">
+                        <li> Permanent hire</li>
+                        <li>Fixed-term contracts</li>
+                        <li>Casual-contracts</li>
+                    </ul>
 
-<ul class="unordered-list">
-<li>Employee performance appraisal and talent identification</li>
-<li>Learning & Development</li>
-<li>Outplacement & support program</li>
-<li>Outsourcing</li>
-<li>On-boarding process</li>
-<li>Job classification, mapping and other strategic HR tools</li>
-<li>Strategic employer branding</li>
-<li>Collective intelligence (team buildings, team spirit workshops, coaching etc.)</li>
-<li>Process consultation on organizational, structural or people matters</li>
-</ul>
+                    <ul class="unordered-list">
+                        <li>Employee performance appraisal and talent identification</li>
+                        <li>Learning & Development</li>
+                        <li>Outplacement & support program</li>
+                        <li>Outsourcing</li>
+                        <li>On-boarding process</li>
+                        <li>Job classification, mapping and other strategic HR tools</li>
+                        <li>Strategic employer branding</li>
+                        <li>Collective intelligence (team buildings, team spirit workshops, coaching etc.)</li>
+                        <li>Process consultation on organizational, structural or people matters</li>
+                    </ul>
 
 
 
-<h4 class="mt-5">We specialize in the following services:</h4>
-<ul class="unordered-list">
-<li>Permanent search, selection and placement services</li>
-<li>Targeted headhunting</li>
-<li>Retainer services</li>
-<li>Project-based recruitment</li>
-<li>Bulk recruitment</li>
-<li>Executive Hiring</li>
-<li>Leadership Hiring</li>
-</ul>
-                        </div>
-                    </div>
+                    <h4 class="mt-5">We specialize in the following services:</h4>
+                    <ul class="unordered-list">
+                        <li>Permanent search, selection and placement services</li>
+                        <li>Targeted headhunting</li>
+                        <li>Retainer services</li>
+                        <li>Project-based recruitment</li>
+                        <li>Bulk recruitment</li>
+                        <li>Executive Hiring</li>
+                        <li>Leadership Hiring</li>
+                    </ul>
                 </div>
             </div>
         </div>
+    </div>
+</div>
 
-<?php include(APPPATH.'views/footer.php') ?>
+<?php include(APPPATH . 'views/footer.php') ?>
--- application/views/more_about_us.php
+++ application/views/more_about_us.php
@@ -1,147 +1,183 @@
-<?php include(APPPATH.'views/header.php') ?>
-
-
-<div class="about-details section-padding30">
-            <div class="container">
-                <div class="row">
-                    <div class="offset-xl-1 col-lg-8">
-                  <div class="about-details-cap mb-50">
-                            <p>Career Foster is a Leading global Executive Search firm with significant presence in
-India in top cities like Hyderabad, Mumbai, Delhi, Bangalore etc. We act as a link
-between our client companies and the potential candidates. At Career Foster, we
-take up the challenge of sourcing the most suited manpower for our client's
-industries just as a partner. We try to increase the productivity of the recruitment
-process at an incredibly low cost. You can be at ease after hiring our services. The
-complete services would be a responsible and accountable exercise.
-We partner with our clients to expand their business potential by providing solutions
-in Executive Search & Selection, Middle Management Hiring, Project Based Hiring
-and Recruitment Process Outsourcing through integrated suite of leadership
-services. With our decade long knowledge expertise in the domain we have been
-successfully able to deliver solutions for all kind of global and local business industry
-specific demands
-                             </p>
-
-
-
-<h4>WHY US!!!</h4>
-
-                             <p>Every recruitment company has its own specialty. We are extremely organized, professional,
-analytic, and human, with fresh insights and a focus on latest trends and market hubs. For us,
-work equals passion and self-actualization, and making our partners enjoy the Recruitment
-Experience. Our Specialty is Leadership Hiring and we’re passionate about how we solve
-recruitment challenges by securing rock star talent for fast growing,emergent and disruptive
-companies. At Career Foster, we provide talent sourcing at the speed of automation.
-</p>
-
-
-<h4>How are we different from other Consultant’s:-</h4>
-<ul class="unordered-list">
-<li>Modern & unique consulting without the silly fees</li>
-<li>Our recruiting know-how ensures targeted talent resourcing</li>
-<li>We're real people working tirelessly to fill your roles</li>
-<li>We successfully source talent across multi-sectors and at all levels</li>
-<li>We employ specialist consultants who are experts on their industries</li>
-<li>We use a range of innovative sourcing solutions to find you the best talent in the market</li>
-<li>We Implement efficient background checks to weed out unqualified candidates</li>
-<li>We Assist clients in streamlining their workforce strategy</li>
-</ul>
-                        </div>
-
-                        <div class="about-details-cap mb-50">
-                          <h4>IT Analytics & Hiring</h4>
-                          <h3>Our Expertise in Information Technology:</h3>
-                          <ul class="unordered-list">
-                          <li>Network Architect</li>
-<li>Systems Architect</li>
-<li>Service Oriented Architecture</li>
-<li>Business Process Management</li>
-<li>Security Analyst</li>
-<li>Penetration Tester</li>
-<li>Cyber Security Engineer</li>
-<li>Information Assurance Analyst</li>
-<li>Data Analyst</li>
-<li>Database Developer</li>
-<li>ERP Developer</li>
-<li>ETL Developer</li>
-<li>Database Administrator (DBA)</li>
-<li>Systems Engineer</li>
-<li>Wireless Engineer</li>
-<li>Network/LAN Engineer</li>
-<li>Telecommunication Engineer</li>
-<li>Project Manager (PMP)</li>
-<li>IT Manager</li>
-<li>Program Manager</li>
-<li>Chief Technology Officer</li>
-<li>508 Compliance</li>
-<li>JavaScript Developer</li>
-<li>Drupal Developer</li>
-<li>SharePoint Developer</li>
-<li>PHP Developer</li>
-<li>WordPress Developer</li>
-<li>Desktop Support Technician</li>
-<li>IT Help Desk (Tier I-III)Deployment</li>
-<li>Java Developer</li>
-<li>NET Developer</li>
-<li>C++ Developer</li>
-</ul>
-
-
+<?php include(APPPATH . 'views/header.php') ?>
+<!--? Hero Start -->
+<div class="slider-area2">
+    <div class="slider-height2 hero-overly2 d-flex align-items-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="hero-cap hero-cap2 text-center">
+                        <h2>More About Us</h2>
                     </div>
-                    <p>With technical recruiting becoming more of a fundamental component for
-business success, companies need a recruiting partner that understands the
-necessary skills and experience to build a successful business.Career Foster
-works as a strategic partner to identify and place the highest caliber of technical
-talent for a wide range of industries. Our specialized IT staffing services division
-assists organizations in attracting, recruiting, and placing qualified candidates
-quickly for single contract consulting, full-time positions, or a large team of IT
-professionals for a long-term project.</p>
-
-
-
-                        <div class="about-details-cap mb-50">
-                          <h3>Our Expertise in Analytics & Data Science:</h3>
-                          <ul class="unordered-list">
-                            <li>Data Analytics</li>
-<li>Data Analyst</li>
-<li>Big Data Analytics</li>
-<li>Data Scientists</li>
-<li>Data Scientist - Predictive Analytics</li>
-<li>Data Analyst with R or Python Analytics</li>
-<li>Data/analytics Platform Development</li>
-<li>Big Data Analytics</li>
-<li>Quality Analyst</li>
-<li>Data Conversional Analyst</li>
-<li>Team Leader-data Analyst/ Senior Data</li>
-<li>Analyst</li>
-<li>Data Analytics(sql& VBA)Lead Data</li>
-<li>Analytics</li>
-<li>Lead Data Analytics Engineer</li>
-<li>Python BI Developer</li>
-<li>Data Analytics</li>
-<li>Business Analyst -</li>
-<li>Business Performance Management</li>
-<li>Data Analytics Lead</li>
-<li>Senior Data Analytics / Research Analyst</li>
-<li>Business Analyst-(Data Science)</li>
-<li>Data Scientist/Performance Analytics</li>
-<li>Data Mining</li>
-<li>Data Analytics- Tableau Developer, Python</li>
-<li>Developer</li>
-
-
-                          </ul>
-                                          <p class="mt-3">In a fast changing world, data & analytics have the power to transform your
-business. The criticality to business can only be justified by talent that
-understands its importance. We know how this function has evolved and that’s
-why we find you experts who can drive your business forward, every single
-day.Finding these people is at the core of our selection process. Our specialists
-identify great talent aligned to your business goals before ensuring a cultural fit
-that’s designed to drive profits for you and satisfaction for your team.
-</p>
-
-
                 </div>
             </div>
         </div>
+    </div>
+</div>
+<!-- Hero End -->
 
-<?php include(APPPATH.'views/footer.php') ?>
+<div class="about-details section-padding30">
+  <div class="container">
+    <div class="row">
+      <div class="offset-xl-1 col-lg-10">
+        <div class="about-details-cap mb-50">
+          <p>Career Foster is a Leading global Executive Search firm with significant presence in
+            India in top cities like Hyderabad, Mumbai, Delhi, Bangalore etc. We act as a link
+            between our client companies and the potential candidates. At Career Foster, we
+            take up the challenge of sourcing the most suited manpower for our client's
+            industries just as a partner. We try to increase the productivity of the recruitment
+            process at an incredibly low cost. You can be at ease after hiring our services. The
+            complete services would be a responsible and accountable exercise.
+            We partner with our clients to expand their business potential by providing solutions
+            in Executive Search & Selection, Middle Management Hiring, Project Based Hiring
+            and Recruitment Process Outsourcing through integrated suite of leadership
+            services. With our decade long knowledge expertise in the domain we have been
+            successfully able to deliver solutions for all kind of global and local business industry
+            specific demands
+          </p>
+
+
+
+          <h4>WHY US!!!</h4>
+
+          <p>Every recruitment company has its own specialty. We are extremely organized, professional,
+            analytic, and human, with fresh insights and a focus on latest trends and market hubs. For us,
+            work equals passion and self-actualization, and making our partners enjoy the Recruitment
+            Experience. Our Specialty is Leadership Hiring and we’re passionate about how we solve
+            recruitment challenges by securing rock star talent for fast growing,emergent and disruptive
+            companies. At Career Foster, we provide talent sourcing at the speed of automation.
+          </p>
+
+
+          <h4>How are we different from other Consultant’s:-</h4>
+          <ul class="unordered-list">
+            <li>Modern & unique consulting without the silly fees</li>
+            <li>Our recruiting know-how ensures targeted talent resourcing</li>
+            <li>We're real people working tirelessly to fill your roles</li>
+            <li>We successfully source talent across multi-sectors and at all levels</li>
+            <li>We employ specialist consultants who are experts on their industries</li>
+            <li>We use a range of innovative sourcing solutions to find you the best talent in the market</li>
+            <li>We Implement efficient background checks to weed out unqualified candidates</li>
+            <li>We Assist clients in streamlining their workforce strategy</li>
+          </ul>
+        </div>
+
+        <div class="about-details-cap mb-50">
+          <h4>IT Analytics & Hiring</h4>
+          <h3>Our Expertise in Information Technology:</h3>
+          <div class="row">
+            <div class="col-sm-4">
+              <ul class="unordered-list">
+                <li>Network Architect</li>
+                <li>Systems Architect</li>
+                <li>Service Oriented Architecture</li>
+                <li>Business Process Management</li>
+                <li>Security Analyst</li>
+                <li>Penetration Tester</li>
+                <li>Cyber Security Engineer</li>
+                <li>Information Assurance Analyst</li>
+                <li>Data Analyst</li>
+                <li>Database Developer</li>
+                <li>ERP Developer</li>
+                <li>ETL Developer</li>
+              </ul>
+            </div>
+            <div class="col-sm-4">
+              <ul class="unordered-list">
+                <li>Database Administrator (DBA)</li>
+                <li>Systems Engineer</li>
+                <li>Wireless Engineer</li>
+                <li>Network/LAN Engineer</li>
+                <li>Telecommunication Engineer</li>
+                <li>Project Manager (PMP)</li>
+                <li>IT Manager</li>
+                <li>Program Manager</li>
+                <li>Chief Technology Officer</li>
+                <li>508 Compliance</li>
+                <li>JavaScript Developer</li>
+              </ul>
+            </div>
+            <div class="col-sm-4">
+              <ul class="unordered-list">
+                <li>Drupal Developer</li>
+                <li>SharePoint Developer</li>
+                <li>PHP Developer</li>
+                <li>WordPress Developer</li>
+                <li>Desktop Support Technician</li>
+                <li>IT Help Desk (Tier I-III)Deployment</li>
+                <li>Java Developer</li>
+                <li>NET Developer</li>
+                <li>C++ Developer</li>
+              </ul>
+            </div>
+          </div>
+
+
+        </div>
+        <p>With technical recruiting becoming more of a fundamental component for
+          business success, companies need a recruiting partner that understands the
+          necessary skills and experience to build a successful business.Career Foster
+          works as a strategic partner to identify and place the highest caliber of technical
+          talent for a wide range of industries. Our specialized IT staffing services division
+          assists organizations in attracting, recruiting, and placing qualified candidates
+          quickly for single contract consulting, full-time positions, or a large team of IT
+          professionals for a long-term project.</p>
+
+
+
+        <div class="about-details-cap mb-50">
+          <h3>Our Expertise in Analytics & Data Science:</h3>
+          <div class="row">
+            <div class="col-sm-4">
+              <ul class="unordered-list">
+                <li>Data Analytics</li>
+                <li>Data Analyst</li>
+                <li>Big Data Analytics</li>
+                <li>Data Scientists</li>
+                <li>Data Scientist - Predictive Analytics</li>
+                <li>Data Analyst with R or Python Analytics</li>
+                <li>Data/analytics Platform Development</li>
+                <li>Big Data Analytics</li>
+                <li>Quality Analyst</li>
+              </ul>
+            </div>
+            <div class="col-sm-4">
+              <ul class="unordered-list">
+                <li>Data Conversional Analyst</li>
+                <li>Team Leader-data Analyst/ Senior Data</li>
+                <li>Analyst</li>
+                <li>Data Analytics(sql& VBA)Lead Data</li>
+                <li>Analytics</li>
+                <li>Lead Data Analytics Engineer</li>
+                <li>Python BI Developer</li>
+                <li>Data Analytics</li>
+                <li>Business Analyst -</li>
+              </ul>
+            </div>
+            <div class="col-sm-4">
+              <ul class="unordered-list">
+                <li>Business Performance Management</li>
+                <li>Data Analytics Lead</li>
+                <li>Senior Data Analytics / Research Analyst</li>
+                <li>Business Analyst-(Data Science)</li>
+                <li>Data Scientist/Performance Analytics</li>
+                <li>Data Mining</li>
+                <li>Data Analytics- Tableau Developer, Python</li>
+                <li>Developer</li>
+              </ul>
+            </div>
+          </div>
+          <p class="mt-3">In a fast changing world, data & analytics have the power to transform your
+            business. The criticality to business can only be justified by talent that
+            understands its importance. We know how this function has evolved and that’s
+            why we find you experts who can drive your business forward, every single
+            day.Finding these people is at the core of our selection process. Our specialists
+            identify great talent aligned to your business goals before ensuring a cultural fit
+            that’s designed to drive profits for you and satisfaction for your team.
+          </p>
+
+
+        </div>
+      </div>
+    </div>
+
+    <?php include(APPPATH . 'views/footer.php') ?>
