a8c7ff1bab891bbf15eddad9ad5f63f2bd958623
gagancool56 <gagancool56@gmail.com>
2021-03-14 18:29:41 +0530

    made title dynamic for all the pages.

--- application/views/header.php
+++ application/views/header.php
@@ -4,7 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title><?= ucwords(uri_string()) ?> </title>
+    <title><?php
+            $uri = $this->uri->segment_array();
+            $url = end($uri);
+            echo $url == '' ? 'Home' : ucwords($url);
+            ?> </title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- <link rel="manifest" href="site.webmanifest"> -->
